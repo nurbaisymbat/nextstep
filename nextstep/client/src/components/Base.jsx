b1571fae28020de0754ea6e31e79025d8606d76f
nurbaisymbat <symbat01011996@gmail.com>
2017-05-27 13:02:36 +0600

    profile

--- nextstep/client/src/components/Base.jsx
+++ nextstep/client/src/components/Base.jsx
@@ -5,31 +5,6 @@ import Auth from '../modules/Auth';
 import axios from 'axios';
 
 class Base extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      user: {
-        myImg: '',
-        name: ''
-      }
-    };
-  }
-
-componentDidMount(){
-  axios.get('/api/getUser',  {
-    responseType: 'json',
-    headers: {
-      'Content-type': 'application/x-www-form-urlencoded',
-      'Authorization': `bearer ${Auth.getToken()}`
-    }
-  })
-    .then(res => {
-        this.setState({
-          user: res.data.user
-        });
-    });
-}
   render() {
     return (
       <div className="row">
@@ -39,12 +14,7 @@ componentDidMount(){
             <h2 className="logo"><span className="next">Next</span><span className="step">Step</span></h2>
           </div>
           <div className="thumbnail text-center" style={{marginBottom: '30%',marginLeft: '-15%'}}>
-          {this.state.user.myImg.length > 0 ?(
-            <Link to="/profile_page"><img src={require('../../../public/userImgs/'+this.state.user.myImg)} className="img-circle" style={{width: '50%'}}/></Link>
-          ):(
             <Link to="/profile_page"><img src={require('../../../public/img/no-user-image.jpg')} className="img-circle" style={{width: '50%'}}/></Link>
-          )}
-            <h5>{this.state.user.name}</h5>
             <h4 className="text-center"><Link to="/profile_page"><span className="glyphicon glyphicon-cog navglyphicon"></span></Link>
             <Link to="/logout"><span className="glyphicon glyphicon-off navglyphicon"></span></Link></h4>
           </div>
